27ab88798fe62f11c8e25d763963f440483d1624
SombreShinobi <sedlan.sibin@gmail.com>
2024-01-25 12:00:21 +0100

    feat: inline some stuff

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@
 
 module Main where
 
+import Data.Functor ((<&>))
 import Data.Time (defaultTimeLocale, getCurrentTime, parseTimeM)
 import Data.Time.Format (formatTime)
 import System.Directory (createDirectoryIfMissing)
@@ -15,12 +16,8 @@ main :: IO ()
 main =
   getArgs >>= \case
     [name, cmd] -> parseCmd cmd name
-    [name, cmd, opt] -> do
-      parseCmd cmd name
-      putStrLn opt
-    _ -> do
-      hPutStrLn stderr "Invalid arguments"
-      exitWith (ExitFailure 1)
+    [name, cmd, opt] -> parseCmd cmd name >> putStrLn opt
+    _ -> hPutStrLn stderr "Invalid arguments" >> exitWith (ExitFailure 1)
 
 parseCmd :: String -> String -> IO ()
 parseCmd cmd name = do
@@ -29,9 +26,7 @@ parseCmd cmd name = do
     "dec" -> putStrLn "Imma dec that for ya"
     "ls" -> readFromFile (fileName name) >>= mapM_ putStrLn
     "c" -> putStrLn "Imma count that for ya"
-    _ -> do
-      hPutStrLn stderr "Invalid command"
-      exitWith (ExitFailure 1)
+    _ -> hPutStrLn stderr "Invalid command" >> exitWith (ExitFailure 1)
 
 inc :: String -> IO ()
 inc file = do
@@ -40,15 +35,10 @@ inc file = do
   writeToFile file formattedTime
 
 writeToFile :: String -> String -> IO ()
-writeToFile file date = do
-  createDirectoryIfMissing True dir
-  appendFile file (date ++ "\n")
+writeToFile file date = createDirectoryIfMissing True dir >> appendFile file (date ++ "\n")
 
 readFromFile :: String -> IO [String]
-readFromFile file = do
-  contents <- readFile file
-  let list = lines contents
-  return list
+readFromFile file = readFile file <&> lines
 
 fileName :: String -> String
 fileName file = dir ++ "/" ++ file ++ ".txt"
